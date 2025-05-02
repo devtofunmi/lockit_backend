51289ebf6d8b4cd2408ca314169dc2e8c8858294
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-05-02 09:57:29 +0100

    fixed prisma table

--- prisma/migrations/20250502080545_add_expires_at_and_viewed/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Message" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "content" TEXT NOT NULL,
-    "viewed" BOOLEAN NOT NULL DEFAULT false,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "expiresAt" DATETIME NOT NULL
-);
